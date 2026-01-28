81908e6923ce32b6fc08467513892e81ba8f0e67
JoshuaStone9 <95473823+JoshuaStone9@users.noreply.github.com>
2026-01-28 23:03:03 +0000

    Update project paths and assembly version in configuration files

--- obj/Debug/net10.0/Project 1.AssemblyInfo.cs
+++ obj/Debug/net10.0/Project 1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project 1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5fefdb28ad2f22a66beb84b3c08da3d710374fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea4fdc48ab8644b97783d28c6e4fe2ab9c479902")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project 1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project 1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
